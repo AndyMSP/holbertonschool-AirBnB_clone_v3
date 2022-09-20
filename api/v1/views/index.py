8e375ec296c0fa167cf8eca7e1883e977fbeda1b
Andy <3775@holbertonschool.com>
2022-09-20 00:19:54 -0400

    Add task 4

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module to define blueprint"""
+
+from api.v1.views import app_views
+from flask import jsonify
+import json
+
+@app_views.route('/status')
+def status():
+    """returns status in json format"""
+    response = jsonify(status='OK')
+    return(response)
