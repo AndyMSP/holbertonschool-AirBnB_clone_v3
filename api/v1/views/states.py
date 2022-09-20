aa000bcdeca31e33802681e9893e85f397edfe60
Andy <3775@holbertonschool.com>
2022-09-20 03:52:16 -0400

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,12 +7,12 @@ from models.state import State
 from models import storage
 
 
-# @state_views.route('/states', strict_slashes=False)
-# def all_states():
-#     """returns all states in json format"""
-#     list_states = []
-#     states_v = storage.all(State).values()
-#     for state in states_v:
-#         list_states.append(state.to_dict())
-#     states_j = jsonify(list_states)
-#     return (states_j)
+@state_views.route('/states', strict_slashes=False)
+def all_states():
+    """returns all states in json format"""
+    list_states = []
+    states_v = storage.all(State).values()
+    for state in states_v:
+        list_states.append(state.to_dict())
+    states_j = jsonify(list_states)
+    return (states_j)
