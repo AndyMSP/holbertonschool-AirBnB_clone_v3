c183475745eba73c64ee72a4066e60ef9a8583f1
Andy <3775@holbertonschool.com>
2022-09-20 01:19:50 -0400

    Add task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,18 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-import json
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status')
@@ -11,3 +22,21 @@ def status():
     """returns status in json format"""
     response = jsonify(status='OK')
     return(response)
+
+
+@app_views.route('/stats')
+def stats():
+    """returns count of each object in storage"""
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    response = jsonify(amenities=storage.count(Amenity),
+                       cities=storage.count(City),
+                       places=storage.count(Place),
+                       reviews=storage.count(Review),
+                       states=storage.count(State),
+                       users=storage.count(User))
+    return (response)
